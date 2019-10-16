78d083d410052d17518f90f53015f075dcf2278c
zheng <765324639@qq.com>
2019-10-17 01:10:24 +0800

    update chapter1

--- createStatementData.js
+++ createStatementData.js
@@ -1,11 +1,4 @@
-const inv =  require('./invoices.json')[0];
-const p = require('./plays.json');
-
-function statement(invoice, plays) {
-    return renderPlainText(createStatementData(invoice, plays));
-}
-
-function createStatementData(invoice, plays) {
+export default function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformances);
@@ -66,24 +59,3 @@ function createStatementData(invoice, plays) {
             .reduce((total, p) => total + p.volumeCredits, 0);
     }
 }
-
-function renderPlainText(data, plays) {
-    let result = `Statement for ${data.customer}\n`;
-    for (let perf of data.performances) {
-        result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
-    }
-
-    result += `Amount owed is ${usd(data.totalAmount)}\n`;
-    result += `You earned ${data.totalVolumeCredits} credits\n`;
-    return result;
-
-    // 格式化数值
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US", 
-            {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber / 100);
-    }
-
-}
-
-let res = statement(inv, p);
-console.log(res);
